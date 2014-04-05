eb44702ba21bc51e34c1d1698cfcf2bcacce20f9
Paul Logston <code@logston.me>
2014-04-05 18:12:41 -0400

    Updated fabfile to python3.

--- fabfile.py
+++ fabfile.py
@@ -1,79 +1,77 @@
+"""
+As of 2014-04-04, Fabric is not supported on python 3+. To
+use this fabfile, call the functions in this file from
+within a python 2.7 env.
+"""
 from fabric.api import local
 
 
-def copy_db_prod_to_staging(prod_app='qsic-production', staging_app='qsic-staging'):
+def transfer_db(source_app='qsic-production', target_app='qsic-staging'):
     """
-    Copy production database from prod_app to staging database
-    in staging_app
+    Copy database from ``source_app`` to ``target_app``.
     """
+
+    # Get HEROKU_POSTGRESQL url aliases.
+    source_db_url = get_heroku_psql_url_name(source_app)
+    target_db_url = get_heroku_psql_url_name(target_app)
 
     # Capture a snapshot of the source database
     #
     # The --expire flag tells pgbackups to automatically expire the
     # oldest manual backup if the retention limit is reached.
-    cmd = ('heroku pgbackups:capture -a %(source_app)s --expire' %
-           {'source_app': prod_app})
+    cmd = 'heroku pgbackups:capture {source_db_url} --app {source_app} --expire'.format(**locals())
     local(cmd)
 
     # Create backup of target app (staging app) database for easy
     # rollback.
-    cmd = ('heroku pgbackups:capture -a %(target_app)s --expire' %
-           {'target_app': staging_app})
+    cmd = 'heroku pgbackups:capture {target_db_url} --app {target_app} --expire'.format(**locals())
     local(cmd)
 
-    # Perform copy
-    #
-    # Restore the most recent backup of source-app to the database
-    # located at psql_url where the color of the handle is the
-    # color of the database handle on the target-app application.
-    cmd = ('heroku pgbackups:restore %(psql_url)s -a %(target_app)s '
-           '`heroku pgbackups:url -a %(source_app)s` '
-           '--confirm %(target_app)s' %
-           {'source_app': prod_app, 'target_app': staging_app,
-            'psql_url': get_heroku_psql_url_name(staging_app)})
+    # Perform transfer.
+    cmd = ('heroku pgbackups:transfer {source_db_url} '
+           '{target_app}::{target_db_url} '
+           '--app {source_app} --confirm {source_app}'.format(**locals()))
     local(cmd)
 
 
 def get_heroku_psql_url_name(app):
-    cmd = 'heroku config | grep HEROKU_POSTGRESQL'
+    """
+    Pull Postgres DB URL alias (of the form HEROKU_POSTGRESQL.+) from heroku config.
+    """
+    cmd = 'heroku config --app {app} | grep HEROKU_POSTGRESQL'.format(app=app)
     config_var = local(cmd, True)
     name = config_var.split(':')[0]
     return name
 
 
-def collectstatic(heroku_app=None):
-    if heroku_app:
-        cmd = ('heroku run python manage.py collectstatic --noinput '
-               '--app %(heroku_app)s' %
-               {'heroku_app': heroku_app})
-    else:
-        cmd = 'python manage.py collectstatic --noinput'
-
+def collectstatic(app):
+    """
+    Run collectstatic on ``app``.
+    """
+    cmd = 'heroku run python manage.py collectstatic --noinput --app {app}'.format(app=app)
     local(cmd)
 
 
-def push(remote_branch=None, local_branch=None, heroku_app=None):
-    if remote_branch and local_branch:
-        cmd = ('git push %(remote_branch)s %(local_branch)s' %
-               {'remote_branch': remote_branch, 'local_branch': local_branch})
+def push(remote, local_branch, heroku_app=None):
+    """
+    Push branch ``local_branch`` up to ``remote`` via git.
+
+    New branch on ``remote`` will be named ``local_branch``.
+    """
+    if remote and local_branch:
+        cmd = ('git push {remote} {local_branch}'.format(**locals()))
         local(cmd)
 
     if heroku_app:
         collectstatic(heroku_app)
 
 
-def push_to_github(local_branch='master'):
-    cmd = ('git push origin %(local_branch)s' %
-           {'local_branch': local_branch})
-    local(cmd)
+def pushto(app):
+    """
+    Push master branch in local git repo up to
+    qsic app specified by ``app``.
+    """
+    push(app, 'master', heroku_app=app)
 
 
-def push_to_staging():
-    push(remote_branch='staging', local_branch='master',
-         heroku_app='qsic-staging')
-
-
-def push_to_production():
-    push(remote_branch='production', local_branch='master',
-         heroku_app='queens-secret')
-
+#TODO add local env vars during deploy
