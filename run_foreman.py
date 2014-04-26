631e1510db8f404ffb27f8debd01148aedf4460f
Paul Logston <code@logston.me>
2014-04-26 17:37:39 -0400

    Fixed CSS bug

--- run_foreman.py
+++ run_foreman.py
@@ -1,6 +1,14 @@
 __author__ = 'paul'
 
+import os
 from subprocess import call
+
+from local.parse_env_file import parse
+
+envvars = parse('local/enter_env.sh')
+
+for k, v in envvars.items():
+    os.environ.update(envvars)
 
 try:
     call(["foreman", "start"])
